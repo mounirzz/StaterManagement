ac7d497816c9a5e0a083982b5fcb44b5f2c31104
Mounir Ez-zahar <mounirzhar@gmail.com>
2018-05-03 16:56:25 +0100

    sync updated user data with session

--- src/main/java/com/micro/demo/controllers/UserController.java
+++ src/main/java/com/micro/demo/controllers/UserController.java
@@ -1,5 +1,14 @@
 package com.micro.demo.controllers;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import javax.imageio.ImageIO;
+import javax.mail.Multipart;
 import javax.validation.Valid;
 
 import org.slf4j.Logger;
@@ -11,10 +20,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
-
 import com.micro.demo.models.User;
 import com.micro.demo.repositories.UserRepository;
 import com.micro.demo.services.MailService;
@@ -109,8 +120,94 @@ public class UserController {
 	}
 	@RequestMapping(value = "/user/activation-send", method = RequestMethod.POST)
 	public ModelAndView activationSendPost(User user, BindingResult result) {
-		User u = userservice.res
+		User u = userservice.resetActivation(user.getEmail());
+		if (u != null) {
+			mailservice.sendNewActivationRequest(u.getEmail(), u.getToken());
+			return new ModelAndView("/user/activation-sent");
+		}else {
+			result.rejectValue("email", "error.doesntExist","We could not find this email in our database");
+			return new ModelAndView("/user/activation-send");
+		}
 	}
+	@RequestMapping("/user/delete")
+	public String delete(Long id) {
+		userservice.delete(id);
+		return "redirect:/user/list";
+	}
+	@RequestMapping("/user/activate")
+	public String activate(String activation) {
+		User u = userservice.activate(activation);
+		if (u != null) {
+			userservice.autologin(u);
+			return "redirect:/";
+		}
+		return "redirect:/error?message=Could not activate with this activation code, please contact support";
+	}
+	@RequestMapping("/user/autologin")
+	public String autologin(User user) {
+		userservice.autologin(user.getUserName());
+		return "redirect:/";
+	}
+	@RequestMapping("/user/edit/{id}")
+	public String edit(@PathVariable("id") Long id , User user) {
+		User u ;
+		User loggedInUser = userservice.getLoggedInUser();
+		if (id == 0) {
+			id = loggedInUser.getId();
+		}
+		if (loggedInUser.getId() != id && !loggedInUser.isAdmin()) {
+			return "user/permission-denied";
+		}else if(loggedInUser.isAdmin()) {
+			u = userRepository.findOne(id);
+		} else {
+			u = loggedInUser;
+		}
+		user.setId(u.getId());
+		user.setUserName(u.getUserName());
+		user.setAddress(u.getAddress());
+		user.setCompanyName(u.getCompanyName());
+		user.setEmail(u.getEmail());
+		user.setFirstname(u.getFirstname());
+		user.setLastname(u.getLastname());
+		
+		return "/user/edit";
+	}
+	@RequestMapping(value= "/user/edit", method = RequestMethod.POST)
+	public String editPost(@Valid User user,BindingResult result) {
+		if (result.hasFieldErrors("email")) {
+			return "/user/edit";
+		}
+		if (userservice.getLoggedInUser().isAdmin()) {
+			userservice.updateUser(user);
+		}else {
+			userservice.updateUser(userservice.getLoggedInUser().getUserName(),user);
+		}
+		if (userservice.getLoggedInUser().getId().equals(user.getId())) {
+			userservice.getLoggedInUser(true);
+		}
+		return "redirect:/user/edit/" +user.getId() + "?updated";
+	}
+	@RequestMapping(value="/user/update", method = RequestMethod.POST)
+	public String handleFileUpload(@RequestParam("file") MultipartFile file) {
+		Format formatter = new SimpleDateFormat("yyyy-MM-dd_HH_mm_ss");
+		String fileName = formatter.format(Calendar.getInstance().getTime())+ "_thumbnail.jpg";
+		User user = userservice.getLoggedInUser();
+		if(!file.isEmpty()){
+			try {
+				String saveDirectory = userRoot + File.separator + user.getId() + File.separator;
+				File test = new File(saveDirectory);
+				if (!test.exists()) {
+					test.mkdirs();
+				}
+				byte[] bytes = file.getBytes();
+				
+				ByteArrayInputStream imageInputStream = new ByteArrayInputStream(bytes);
+				BufferedImage image = ImageIO.read(imageInputStream);
+				BufferedImage thumbnail = Scalr.
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+		}
 	
 	
 	
--- src/main/java/com/micro/demo/repositories/UserRepository.java
+++ src/main/java/com/micro/demo/repositories/UserRepository.java
@@ -4,10 +4,12 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.micro.demo.models.User;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 	User findOneByUserName(String name);
 
